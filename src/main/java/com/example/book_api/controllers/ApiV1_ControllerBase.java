2e0665035dcea4ba30d5c972aa414f10f6572e57
Yaroslav Kushnirenko <kushnirenko.ya@yandex.ru>
2025-09-10 20:14:13 +0300

    Added the controller base with the common versioned API prefix for URIs

---
+++ src/main/java/com/example/book_api/controllers/ApiV1_ControllerBase.java
@@ -0,0 +1,6 @@
+package com.example.book_api.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("api/v1")
+public interface ApiV1_ControllerBase { }
--- src/main/java/com/example/book_api/controllers/BookRestControllerApi.java
+++ src/main/java/com/example/book_api/controllers/BookRestControllerApi.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -15,20 +14,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Tag(name = "Books", description = "The API for book management")
-public interface BookRestControllerApi {
+public interface BookRestControllerApi extends ApiV1_ControllerBase {
+    String JSON = "application/json";
+
+    @PostMapping(path = "books", produces = JSON, consumes = JSON)
     @Operation(summary = "Creating a new book", description = "The API method to create a new book")
-    @ApiResponses({
-            @ApiResponse(responseCode = "201", description = "The book was created",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = PartialBookResponse.class))),
-            @ApiResponse(responseCode = "400", description = "Uncorrected response data",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Object.class))),
-            @ApiResponse(responseCode = "409", description = "A book with the same ISBN already exists",
-                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Object.class))),
-    })
-    @PostMapping(value = "/api/v1/books", produces = "application/json", consumes = "application/json")
-    ResponseEntity<PartialBookResponse> createBook(
-            @Parameter(description = "The book data", required = true)
-            @Valid @RequestBody
-            WholeBookRequest req);
+    @ApiResponse(responseCode = "201", description = "The book was created", content = @Content(mediaType = JSON, schema = @Schema(implementation = PartialBookResponse.class)))
+    @ApiResponse(responseCode = "400", description = "Uncorrected response data", content = @Content(mediaType = JSON, schema = @Schema(implementation = Object.class)))
+    @ApiResponse(responseCode = "409", description = "A book with the same ISBN already exists", content = @Content(mediaType = JSON, schema = @Schema(implementation = Object.class)))
+    ResponseEntity<PartialBookResponse> createBook(@Parameter(description = "The book data", required = true) @Valid @RequestBody WholeBookRequest req);
 }
 
